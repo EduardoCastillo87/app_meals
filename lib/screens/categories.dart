47d2b91319c7c80c0617270393082030255ed613
Eduardo Castillo <eduardo.infoline@gmail.com>
2025-05-26 18:17:41 -0500

    Add categories feature with data model and grid display. Introduced Category model, dummy data, and CategoriesScreen with CategoryGridItem for visual representation.

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
+import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -8,19 +11,16 @@ class CategoriesScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Pick your category')),
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: const [
-          Text('1'),
-          Text('2'),
-          Text('3'),
-          Text('4'),
-          Text('5'),
-          Text('6'),
+        children: [
+          for (final Category in availableCategories)
+            CategoryGridItem(category: Category),
         ],
       ),
     );
