740c9bb7208fc6b2845ab2d2034e20ec68b6bd7b
Eduardo Castillo <eduardo.infoline@gmail.com>
2025-05-27 18:58:47 -0500

    Add category selection functionality to CategoriesScreen, enabling navigation to MealScreen with filtered meals based on selected category.

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/category.dart';
+import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) =>
+            MealScreen(title: category.title, meals: filteredMeals),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +35,12 @@ class CategoriesScreen extends StatelessWidget {
         ),
         children: [
           for (final Category in availableCategories)
-            CategoryGridItem(category: Category),
+            CategoryGridItem(
+              category: Category,
+              onSelectCategory: () {
+                _selectCategory(context, Category);
+              },
+            ),
         ],
       ),
     );
