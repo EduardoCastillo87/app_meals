bd0ca4cc113b94b5f09ee2f8d7d441b66fa80979
Eduardo Castillo <eduardo.infoline@gmail.com>
2025-05-28 18:16:25 -0500

    Make title parameter in MealScreen optional and handle null case in app bar title rendering.

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -4,9 +4,9 @@ import 'package:meals/screens/meal_details.dart';
 import 'package:meals/widgets/meal_item.dart';
 
 class MealScreen extends StatelessWidget {
-  const MealScreen({super.key, required this.title, required this.meals});
+  const MealScreen({super.key, this.title, required this.meals});
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
 
   void selectMeal(BuildContext context, Meal meal) {
@@ -49,8 +49,13 @@ class MealScreen extends StatelessWidget {
         ),
       );
     }
+
+    if (title == null) {
+      return content;
+    }
+
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
+      appBar: AppBar(title: Text(title!)),
       body: content,
     );
   }
